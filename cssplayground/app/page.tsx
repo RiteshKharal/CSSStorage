b221fc07df1519b0de76a05001062a839b0e0e2c
Ritesh kharal <kharalritesh@gmail.com>
2026-02-01 19:27:29 +0545

    ...

--- cssplayground/app/page.tsx
+++ cssplayground/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold p-10">In Progress</h1>
+      <h1 className="text-3xl font-bold p-10">In Progress...</h1>
     </div>
   );
 }
